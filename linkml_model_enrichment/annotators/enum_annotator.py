a2f58664a1560098acd56cc771a81b36d84e1a52
Mark Andrew Miller <MAM@lbl.gov>
2021-09-10 12:03:21 -0400

    handle 4xx errors from OLS

--- linkml_model_enrichment/annotators/enum_annotator.py
+++ linkml_model_enrichment/annotators/enum_annotator.py
@@ -126,97 +126,100 @@ def one_enum_to_ols_frame_list(permitteds, one_enum_param):
             term_retr_base = 'http://www.ebi.ac.uk/ols/api/ontologies/'
             term_retr_assembled = term_retr_base + ols_string_search_res_row.ontology_name + '/terms/' + twice
             term_details = requests.get(term_retr_assembled)
-            term_json = term_details.json()
-            has_label = 'label' in set(term_json.keys())
-            if has_label:
-                logger.debug(term_retr_assembled)
-                temp = term_json['label']
-                logger.debug(temp)
-                label_frame = pds.DataFrame([[term_json['label'], 'label', 'label', '']],
-                                            columns=['name', 'scope', 'type', 'xrefs'])
-                label_frame['obo_id'] = term_json['obo_id']
-                label_frame['pref_lab'] = term_json['label']
-                annotations_frame = annotations_frame.append(label_frame, ignore_index=True)
-            # also get other properties?
 
-            has_synonyms = 'obo_synonym' in set(term_json.keys())
-            if has_synonyms:
-                obo_syn_json = term_json['obo_synonym']
-                obo_syn_frame = pds.DataFrame(obo_syn_json)
-                obo_syn_frame['obo_id'] = term_json['obo_id']
-                obo_syn_frame['pref_lab'] = term_json['label']
-                annotations_frame = annotations_frame.append(obo_syn_frame, ignore_index=True)
+            if term_details.status_code == 200:
+                term_json = term_details.json()
+                has_label = 'label' in set(term_json.keys())
+                if has_label:
+                    logger.debug(term_retr_assembled)
+                    temp = term_json['label']
+                    logger.debug(temp)
+                    label_frame = pds.DataFrame([[term_json['label'], 'label', 'label', '']],
+                                                columns=['name', 'scope', 'type', 'xrefs'])
+                    label_frame['obo_id'] = term_json['obo_id']
+                    label_frame['pref_lab'] = term_json['label']
+                    annotations_frame = annotations_frame.append(label_frame, ignore_index=True)
+                # also get other properties?
 
-            # # don't process every kind of annotation, like genetic code
-            # has_annotations = 'annotation' in set(term_json.keys())
-            # if has_annotations:
-            #     obo_ano_json = term_json['annotation']
-            #     for anokey in obo_ano_json.keys():
-            #         for keyval in obo_ano_json[anokey]:
-            #             new_row = {'name': keyval,
-            #                        'obo_id': term_json['obo_id'],
-            #                        'scope': anokey,
-            #                        'type': 'annotation',
-            #                        'xrefs': '',
-            #                        'pref_lab': term_json['label']}
-            #             annotations_frame = annotations_frame.append(new_row, ignore_index=True)
+                has_synonyms = 'obo_synonym' in set(term_json.keys())
+                if has_synonyms:
+                    obo_syn_json = term_json['obo_synonym']
+                    obo_syn_frame = pds.DataFrame(obo_syn_json)
+                    obo_syn_frame['obo_id'] = term_json['obo_id']
+                    obo_syn_frame['pref_lab'] = term_json['label']
+                    annotations_frame = annotations_frame.append(obo_syn_frame, ignore_index=True)
 
-            annotations_row_count = len(annotations_frame.index)
+                # # don't process every kind of annotation, like genetic code
+                # has_annotations = 'annotation' in set(term_json.keys())
+                # if has_annotations:
+                #     obo_ano_json = term_json['annotation']
+                #     for anokey in obo_ano_json.keys():
+                #         for keyval in obo_ano_json[anokey]:
+                #             new_row = {'name': keyval,
+                #                        'obo_id': term_json['obo_id'],
+                #                        'scope': anokey,
+                #                        'type': 'annotation',
+                #                        'xrefs': '',
+                #                        'pref_lab': term_json['label']}
+                #             annotations_frame = annotations_frame.append(new_row, ignore_index=True)
 
-            if annotations_row_count == 0:
-                logger.warning('NO ANNOTATIONS')
-                manual_row = pds.Series(['', '', '', '', '', ''])
-                row_df = pds.DataFrame([manual_row], columns=['name', 'obo_id', 'scope', 'type', 'xrefs', 'pref_lab'])
-                annotations_frame = pds.concat([row_df, annotations_frame], ignore_index=True)
-                failures.append(orig_enum)
-            annotations_frame['enum_class'] = one_enum_param
-            annotations_frame['query'] = tidied_enum
-            annotations_frame['orig_enum'] = orig_enum
-            # check whether anny of the annotation on any of the hits have an
-            #   acceptable cosine string distance
-            annotations_frame['name'] = annotations_frame['name'].fillna('')
-            annotations_frame['cosine_dist'] = \
-                annotations_frame.apply(lambda row: inner_cosine_obj.distance(tidied_enum.strip().lower(),
-                                                                              row['name'].strip().lower()),
-                                        axis=1)
-            annotations_frame = annotations_frame.sort_values('cosine_dist')
-            annotations_frame['dist_ok'] = annotations_frame['cosine_dist'] <= mdg
-            annotations_frame['rank'] = list(range(1, len(annotations_frame.index)+1))
+                annotations_row_count = len(annotations_frame.index)
 
-            # annotations_frame = annotations_frame[
-            #     ['enum_class', 'orig_enum', 'query', 'name', 'cosine_dist', 'dist_ok',
-            #      'obo_id', 'pref_lab', 'type', 'scope']]
+                if annotations_row_count == 0:
+                    logger.warning('NO ANNOTATIONS')
+                    manual_row = pds.Series(['', '', '', '', '', ''])
+                    row_df = pds.DataFrame([manual_row], columns=['name', 'obo_id', 'scope', 'type', 'xrefs', 'pref_lab'])
+                    annotations_frame = pds.concat([row_df, annotations_frame], ignore_index=True)
+                    failures.append(orig_enum)
+                annotations_frame['enum_class'] = one_enum_param
+                annotations_frame['query'] = tidied_enum
+                annotations_frame['orig_enum'] = orig_enum
+                # check whether anny of the annotation on any of the hits have an
+                #   acceptable cosine string distance
+                annotations_frame['name'] = annotations_frame['name'].fillna('')
+                annotations_frame['cosine_dist'] = \
+                    annotations_frame.apply(lambda row: inner_cosine_obj.distance(tidied_enum.strip().lower(),
+                                                                                  row['name'].strip().lower()),
+                                            axis=1)
+                annotations_frame = annotations_frame.sort_values('cosine_dist')
+                annotations_frame['dist_ok'] = annotations_frame['cosine_dist'] <= mdg
+                annotations_frame['rank'] = list(range(1, len(annotations_frame.index)+1))
 
-            annotations_frame = annotations_frame[cols2display]
+                # annotations_frame = annotations_frame[
+                #     ['enum_class', 'orig_enum', 'query', 'name', 'cosine_dist', 'dist_ok',
+                #      'obo_id', 'pref_lab', 'type', 'scope']]
 
-            # do something with xrefs?
+                annotations_frame = annotations_frame[cols2display]
+
+                # do something with xrefs?
         logger.debug(annotations_frame)
 
         # get best acceptable row
-        acceptable_cosine = annotations_frame[annotations_frame['cosine_dist'] <= mdg]
-        acceptable_row_count = len(acceptable_cosine.index)
-        if acceptable_row_count > 0:
-            best_acceptable = acceptable_cosine.iloc[0]
-            success_frame = success_frame.append(best_acceptable)
-            # check if permitted value already has a meaning
-            meaning_search = list(inferred_model['enums'][one_enum_param]['permissible_values'][orig_enum].keys())
-            if 'meaning' in meaning_search:
-                has_meaning = True
+        if "cosine_dist" in annotations_frame.columns:
+            acceptable_cosine = annotations_frame[annotations_frame['cosine_dist'] <= mdg]
+            acceptable_row_count = len(acceptable_cosine.index)
+            if acceptable_row_count > 0:
+                best_acceptable = acceptable_cosine.iloc[0]
+                success_frame = success_frame.append(best_acceptable)
+                # check if permitted value already has a meaning
+                meaning_search = list(inferred_model['enums'][one_enum_param]['permissible_values'][orig_enum].keys())
+                if 'meaning' in meaning_search:
+                    has_meaning = True
+                else:
+                    has_meaning = False
+                meaningless = not has_meaning
+                if meaningless or omg:
+                    # insert meaning
+                    inferred_model['enums'][one_enum_param]['permissible_values'][orig_enum]['meaning'] = best_acceptable[
+                        'obo_id']
+                    inferred_model['enums'][one_enum_param]['permissible_values'][orig_enum]['description'] = \
+                        best_acceptable['pref_lab']
             else:
-                has_meaning = False
-            meaningless = not has_meaning
-            if meaningless or omg:
-                # insert meaning
-                inferred_model['enums'][one_enum_param]['permissible_values'][orig_enum]['meaning'] = best_acceptable[
-                    'obo_id']
-                inferred_model['enums'][one_enum_param]['permissible_values'][orig_enum]['description'] = \
-                    best_acceptable['pref_lab']
-        else:
-            temp = 'NO ACCEPTABLE MAPPINGS FOR ' + one_enum_param + " " + orig_enum
-            logger.warning(temp)
-            # sort and make unique
-            failures.append(orig_enum)
-        per_enum_frame = per_enum_frame.append(annotations_frame)
+                temp = 'NO ACCEPTABLE MAPPINGS FOR ' + one_enum_param + " " + orig_enum
+                logger.warning(temp)
+                # sort and make unique
+                failures.append(orig_enum)
+            per_enum_frame = per_enum_frame.append(annotations_frame)
     # I think there will be one success frame for each enum
     success_frame = success_frame[cols2display]
     success_frame = success_frame[list(annotations_frame.columns)]
