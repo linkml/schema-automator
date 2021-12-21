6afe076e842dea81ba8979d710bf7157179e1e77
cmungall <cjm@berkeleybop.org>
2021-12-21 15:49:29 -0800

    new

--- tests/test_from_tsv.py
+++ tests/test_from_tsv.py
@@ -5,6 +5,8 @@
 import unittest
 import os
 import yaml
+from linkml_runtime.linkml_model import SchemaDefinition
+
 from linkml_model_enrichment.importers.csv_import_engine import CsvDataImportEngine
 from linkml.generators.yamlgen import YAMLGenerator
 from tests import INPUT_DIR, OUTPUT_DIR
@@ -18,10 +20,10 @@ NEWSLOTS_OUTSCHEMA = os.path.join(OUTPUT_DIR, 'new_slots.yaml')
 
 
 class TestTsvmport(unittest.TestCase):
-    """TSV """
+    """TSV import """
 
     def test_tsv(self):
-        """Test that expando can be imported."""
+        """Test that TSVs can be imported to LinkML."""
         ie = CsvDataImportEngine()
         schema_dict = ie.convert(BOOKS, class_name='Book', name='books')
         ys = yaml.dump(schema_dict, default_flow_style=False, sort_keys=False)
@@ -40,3 +42,21 @@ class TestTsvmport(unittest.TestCase):
         ys = yaml.dump(schema_dict, default_flow_style=False, sort_keys=False)
         with open(NEWSLOTS_OUTSCHEMA, 'w') as stream:
             stream.write(ys)
+
+    def _convert(self, base_name: str, cn='Example', index_slot='examples') -> SchemaDefinition:
+        ie = CsvDataImportEngine()
+        fn = f'{base_name}.tsv'
+        infile = os.path.join(INPUT_DIR, fn)
+        schema_dict = ie.convert(infile, class_name=cn, name=index_slot)
+        ys = yaml.dump(schema_dict, default_flow_style=False, sort_keys=False)
+        outfile = os.path.join(OUTPUT_DIR, f'{base_name}.yaml')
+        with open(outfile, 'w') as stream:
+            stream.write(ys)
+        s = YAMLGenerator(ys).serialize()
+        outfile2 = os.path.join(OUTPUT_DIR, f'{base_name}.enhanced.yaml')
+        with open(outfile2, 'w') as stream:
+            stream.write(s)
+        return s
+
+    def test_tsv_dad_is_metadata(self):
+        self._convert('dad-is-metadata')
