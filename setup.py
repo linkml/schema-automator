c33848361409018ff0ef27ea34f0426bc6467c60
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2022-04-14 17:20:13 -0700

    change github action to use setup.py for pypi dist

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ URL = 'https://github.com/linkml/schema_automator'
 AUTHOR = 'Mark Miller'
 EMAIL = 'mam@lbl.gov'
 REQUIRES_PYTHON = '>=3.7.0'
-VERSION = '1.0.0b0'
+VERSION = '0.1.3'
 LICENSE = 'BSD'
 
 with open("requirements.txt", "r") as FH:
