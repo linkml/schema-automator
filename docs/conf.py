9326041d00da2e4590c64a370dd914d88a3776ef
Harshad Hegde <hegdehb@gmail.com>
2022-04-23 09:42:21 -0500

    change theme

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.githubpages',
     'sphinx.ext.autosectionlabel',
-    'cloud_sptheme',
+    'sphinx-pdj-theme',
     'sphinx_click',
     'sphinx.ext.viewcode',
     'sphinx.ext.todo',
@@ -64,7 +64,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'cloud'
+html_theme = 'sphinx_pdj_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
