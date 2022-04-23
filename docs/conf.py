8c3107a2628cd120e0f2480bcf14284c5ab3f2d2
Harshad Hegde <hegdehb@gmail.com>
2022-04-23 09:37:18 -0500

    html_theme was incorrect

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 
 project = 'Schema Automator'
 copyright = '2022, Chris Mungall'
-author = 'Chris Mungall'
+author = 'Chris Mungall, Harshad Hegde'
 
 # The full version, including alpha/beta/rc tags
 # release = '0.1.4'
@@ -64,7 +64,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'cloud_sptheme'
+html_theme = 'cloud'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
