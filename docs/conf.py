7446b237a6e7481ccad80996eb7449d63424bd18
Harshad Hegde <hegdehb@gmail.com>
2022-04-23 08:44:27 -0500

    updated them in conf

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.githubpages',
     'sphinx.ext.autosectionlabel',
-    'sphinx_rtd_theme',
+    'groundwork-sphinx-theme',
     'sphinx_click',
     'sphinx.ext.viewcode',
     'sphinx.ext.todo',
@@ -64,7 +64,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'groundwork'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
