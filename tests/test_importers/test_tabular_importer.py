64adb827b3d836cee8d3e5cc5849a40731636ab2
cmungall <cjm@berkeleybop.org>
2023-01-31 11:19:21 -0800

    skip beautiful soup test on windows

--- tests/test_importers/test_tabular_importer.py
+++ tests/test_importers/test_tabular_importer.py
@@ -2,6 +2,7 @@
 
 import unittest
 import os
+from sys import platform
 
 import pandas as pd
 
@@ -12,12 +13,14 @@ from tests import INPUT_DIR, OUTPUT_DIR
 OUT = os.path.join(OUTPUT_DIR, "BioMRLs.yaml")
 INPUT_HTML = os.path.join(INPUT_DIR, "BioMRLs-table.html")
 
+
 class TestTableImporter(unittest.TestCase):
     """Tests import from tables data packages """
 
     def setUp(self) -> None:
         pass
 
+    @unittest.skipIf(platform.startswith("win"), "Do not run on Windows")
     def test_import(self):
         """
         Test importing a table from HTML via BS
@@ -25,9 +28,11 @@ class TestTableImporter(unittest.TestCase):
         ie = TableImportEngine(parent="BioMRL",
                                element_type="enum",
                                columns=["permissible_value", "description"])
+        # this doesn't work on windows:
         dfs = pd.read_html(f"file://{INPUT_HTML}")
         schema = ie.import_from_dataframe(dfs[0])
         write_schema(schema, OUT)
+
 
 
 
