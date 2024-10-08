71c0099c807fe9fac04a3ef82c26b3e4a9ff10f0
David Linke <dr.david.linke@gmail.com>
2024-10-08 08:15:04 +0200

    WIP (sync between work places)

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,6 @@
 import os
-import pprint
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
 INPUT_DIR = os.path.join(ROOT, 'resources')
 OUTPUT_DIR = os.path.join(ROOT, 'outputs')
 MODEL_DIR = os.path.join(ROOT, 'test_models')
-
-
