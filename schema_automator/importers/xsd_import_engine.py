be9d6e8429b3bb50edd6b332b38b131f273b476c
Michael Milton <michael.r.milton@gmail.com>
2024-12-20 18:05:06 +1100

    Walk upwards from elements

--- schema_automator/importers/xsd_import_engine.py
+++ schema_automator/importers/xsd_import_engine.py
@@ -27,7 +27,9 @@ TYPE_MAP = {
     "string": "String",
     "time": "Time",
     "int": "Integer",
-    "integer": "Integer"
+    "integer": "Integer",
+    # NMTOKEN is basically just a string inside an attribute
+    "NMTOKEN": "String"
 }
 MISSING_TYPES = {
     "base64Binary",
@@ -98,40 +100,88 @@ def element_to_linkml_type(el: etree.ElementBase) -> str:
     """
     return xsd_to_linkml_type(resolve_type(el))
 
+def find_class_name(el: etree.ElementBase, root_name: str = "Root") -> str:
+    """
+    Returns the name of the class that this element is a slot for
+
+    Params:
+        el: the element that will be a slot for the class whose name we want to find
+        root_name: class name to assign to the schema's root
+    """
+    for parent in el.iterancestors():
+        if "name" in parent.attrib:
+            return parent.attrib["name"]
+        elif parent.tag == f"{{{XSD}}}schema":
+            return root_name
+    raise ValueError("Could not find class name for element")
+
+def visit_element(el: etree.ElementBase, sb: SchemaBuilder, target_ns: str | None) -> None:
+    """
+    Adds an XSD element as an attribute to a class in the schema
+
+    Params:
+        el: the <xsd:element> to add
+        sb: the schema builder to add the element to
+    """
+    cls_name = find_class_name(el)
+    attrib_name = formatutils.lcamelcase(el.attrib['name'])
+    if cls_name in sb.schema.classes:
+        cls = sb.schema.classes[cls_name]
+    else:
+        cls = ClassDefinition(
+            cls_name,
+            class_uri=urljoin(target_ns, cls_name) if target_ns else None,
+        )
+        sb.add_class(cls)
+
+    cls.attributes[attrib_name] = SlotDefinition(
+        name=attrib_name,
+        slot_uri=urljoin(cls.class_uri, el.attrib['name']) if cls.class_uri else None,
+        range=element_to_linkml_type(el)
+    )
+
 @dataclass
 class XsdImportEngine(ImportEngine):
 
-    def el_to_class(self, el: etree.ElementBase, name: str, target_ns: str | None) -> ClassDefinition:
-        attributes: dict[str, SlotDefinition] = {}
-        # Find all child elements and add them as slots
-        for el in el.findall(f".//xsd:element" , namespaces=NAMESPACES):#, namespaces=NAMESPACES):
-            attrib_name = formatutils.lcamelcase(el.attrib['name']) 
-            attributes[attrib_name] = SlotDefinition(
-                name=attrib_name,
-                slot_uri=urljoin(target_ns, el.attrib['name']) if target_ns else None,
-                range=element_to_linkml_type(el),
-            )
-        # Find all attributes and add them as slots
-        for el in el.findall(f".//xsd:attribute", namespaces=NAMESPACES):
-            attrib_name = formatutils.lcamelcase(el.attrib['name']) 
-            attributes[attrib_name] = SlotDefinition(
-                name=attrib_name,
-                range=element_to_linkml_type(el),
-            )
+
+    # def el_to_class(self, el: etree.ElementBase, name: str, target_ns: str | None) -> ClassDefinition:
+    #     attributes: dict[str, SlotDefinition] = {}
+    #     # Find all child elements and add them as slots
+    #     for el in el.findall(f".//xsd:element" , namespaces=NAMESPACES):#, namespaces=NAMESPACES):
+    #         attrib_name = formatutils.lcamelcase(el.attrib['name']) 
+    #         attributes[attrib_name] = SlotDefinition(
+    #             name=attrib_name,
+    #             slot_uri=urljoin(target_ns, el.attrib['name']) if target_ns else None,
+    #             range=element_to_linkml_type(el),
+    #         )
+    #     # Find all attributes and add them as slots
+    #     for el in el.findall(f".//xsd:attribute", namespaces=NAMESPACES):
+    #         attrib_name = formatutils.lcamelcase(el.attrib['name']) 
+    #         attributes[attrib_name] = SlotDefinition(
+    #             name=attrib_name,
+    #             range=element_to_linkml_type(el),
+    #         )
         
-        return ClassDefinition(
-            name=formatutils.camelcase(name),
-            class_uri=urljoin(target_ns, name) if target_ns else None,
-            attributes=attributes
-        )
+    #     return ClassDefinition(
+    #         name=formatutils.camelcase(name),
+    #         class_uri=urljoin(target_ns, name) if target_ns else None,
+    #         attributes=attributes
+    #     )
+
+
 
     def convert(self, file: str, **kwargs) -> SchemaDefinition:
         sb = SchemaBuilder()
-        # tree = ElementTree.parse(file)
-        tree: etree._ElementTree = etree.parse(file)
+        tree = etree.parse(file)
         target_ns: str | None = tree.getroot().attrib.get('targetNamespace')
-        sb.add_class(self.el_to_class(tree.getroot(), "Root", target_ns))
-        for typ in tree.findall("//xsd:complexType[@name]", namespaces=NAMESPACES):
-            sb.add_class(self.el_to_class(typ, typ.attrib['name'], target_ns))
+        for element in tree.findall("//xsd:element", namespaces=NAMESPACES):
+            visit_element(element, sb, target_ns=target_ns)
+        for element in tree.findall("//xsd:attribute", namespaces=NAMESPACES):
+            visit_element(element, sb, target_ns=target_ns)
+        # tree: etree._ElementTree = etree.parse(file)
+        # target_ns: str | None = tree.getroot().attrib.get('targetNamespace')
+        # sb.add_class(self.el_to_class(tree.getroot(), "Root", target_ns))
+        # for typ in tree.findall("//xsd:complexType[@name]", namespaces=NAMESPACES):
+        #     sb.add_class(self.el_to_class(typ, typ.attrib['name'], target_ns))
 
         return sb.schema
